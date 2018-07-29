cf665479f285b2378214e5f9e3eb006bcc945024
disc0ninja <disc0ninja77@gmail.com>
2018-07-29 10:48:01 -0500

    Options integrated

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,70 +1,102 @@
 'use strict';
 
-console.log('App.js is running!');
+// App Target
+var appRoot = document.getElementById('app');
 
+// App Data
 var app = {
     title: 'Indecision App',
     subtitle: 'Are you feeling lucky?',
-    options: ['One', 'Two']
+    options: []
+
+    // Options Form Submission
+};var onFormSubmit = function onFormSubmit(e) {
+    // Stop full page refresh
+    e.preventDefault();
+
+    var option = e.target.elements.option.value;
+
+    if (option) {
+        app.options.push(option);
+        e.target.elements.option.value = '';
+        console.log(app.options);
+        renderApplication();
+    }
+
+    console.log(option);
 };
 
-var template = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
-        null,
-        app.title
-    ),
-    app.subtitle && React.createElement(
-        'p',
-        null,
-        app.subtitle
-    ),
-    app.options.length > 0 ? React.createElement(
-        'p',
-        null,
-        'Here are your options'
-    ) : React.createElement(
-        'p',
-        null,
-        'No Options'
-    )
-);
-var appRoot = document.getElementById('app');
-
-var user = {
-    name: 'Billy',
-    age: '31',
-    location: 'New Zealand'
+var onMakeDecision = function onMakeDecision() {
+    var randomNum = Math.floor(Math.random() * app.options.length);
+    var option = app.options[randomNum];
+    alert(option);
 };
 
-var getLocation = function getLocation(location) {
-    if (location) {
-        return React.createElement(
+// Clear options from app{}
+var removeAll = function removeAll() {
+    app.options = [];
+    renderApplication();
+};
+
+// Render Application Function
+var renderApplication = function renderApplication() {
+    var template = React.createElement(
+        'div',
+        null,
+        React.createElement(
+            'h1',
+            null,
+            app.title
+        ),
+        app.subtitle && React.createElement(
             'p',
             null,
-            'Location: ',
-            location
-        );
-    }
+            app.subtitle
+        ),
+        app.options.length > 0 ? React.createElement(
+            'p',
+            null,
+            'Here are your options'
+        ) : React.createElement(
+            'p',
+            null,
+            'No Options'
+        ),
+        React.createElement(
+            'button',
+            { disabled: app.options.length === 0, onClick: onMakeDecision },
+            'What should I do?'
+        ),
+        React.createElement(
+            'button',
+            { onClick: removeAll },
+            'Remove All'
+        ),
+        React.createElement(
+            'ul',
+            null,
+            app.options.map(function (opt) {
+                return React.createElement(
+                    'li',
+                    { key: opt },
+                    opt
+                );
+            })
+        ),
+        React.createElement(
+            'form',
+            { onSubmit: onFormSubmit },
+            React.createElement('input', { type: 'text', name: 'option' }),
+            React.createElement(
+                'button',
+                { onClick: renderApplication },
+                'Add Option'
+            )
+        )
+    );
+
+    ReactDOM.render(template, appRoot);
 };
 
-var templateTwo = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
-        null,
-        user.name ? user.name : 'Anon'
-    ),
-    user.age >= 18 && React.createElement(
-        'p',
-        null,
-        'Age:  ',
-        user.age
-    ),
-    getLocation(user.location)
-);
-
-ReactDOM.render(template, appRoot);
+// Start Application
+renderApplication();
--- src/app.js
+++ src/app.js
@@ -1,39 +1,67 @@
-console.log('App.js is running!');
+// App Target
+const appRoot = document.getElementById('app');
 
+// App Data
 const app = {
     title: 'Indecision App',
     subtitle: 'Are you feeling lucky?',
-    options: ['One', 'Two']
+    options: []
 }
 
-const template = (
-    <div>
-        <h1>{app.title}</h1>
-        {app.subtitle && <p>{app.subtitle}</p>}
-        {app.options.length > 0 ? <p>Here are your options</p> : <p>No Options</p>}
-        </div>
-);
-const appRoot = document.getElementById('app');
+// Options Form Submission
+const onFormSubmit = (e) => {
+    // Stop full page refresh
+    e.preventDefault();
 
-const user = {
-    name: 'Billy',
-    age: '31',
-    location: 'New Zealand'
+    const option = e.target.elements.option.value
+
+    if (option) {
+        app.options.push(option);
+        e.target.elements.option.value = '';
+        console.log(app.options);
+        renderApplication();
+    }
+
+    console.log(option);
 };
 
-const getLocation = (location) => {
-    if (location) {
-        return <p>Location: {location}</p>
-    }
-}
+const onMakeDecision = () => {
+    const randomNum = Math.floor(Math.random() * app.options.length);
+    const option = app.options[randomNum];
+    alert(option);
+};
 
+// Clear options from app{}
+const removeAll = () => {
+    app.options = [];
+    renderApplication();
+};
 
-const templateTwo = (
-    <div>
-        <h1>{user.name ? user.name : 'Anon'}</h1>
-        {user.age >= 18 && <p>Age:  {user.age}</p>}
-        {getLocation(user.location)}
-    </div>
-);
+// Render Application Function
+const renderApplication = () => {
+    const template = (
+        <div>
+            <h1>{app.title}</h1>
+            {app.subtitle && <p>{app.subtitle}</p>}
+            {app.options.length > 0 ? <p>Here are your options</p> : <p>No Options</p>}
+            <button disabled={app.options.length === 0} onClick={onMakeDecision}>What should I do?</button>
+            <button onClick={removeAll}>Remove All</button>
+            <ul>
+                {
+                    app.options.map( (opt) => {
+                        return (<li key={opt}>{opt}</li>);
+                    })
+                }
+            </ul>
+            <form onSubmit={onFormSubmit}>
+                <input type='text' name='option' />
+                <button onClick={renderApplication}>Add Option</button>
+            </form>
+            </div>
+    );
 
-ReactDOM.render(template, appRoot);
+    ReactDOM.render(template, appRoot);
+};
+
+// Start Application
+renderApplication();
